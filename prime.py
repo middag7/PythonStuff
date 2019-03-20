8c7f17b73990e615fe17d7fb4806e13aeb2bc37a
middag7 <lcma@st-andrews.ac.uk>
2019-03-20 09:29:12 +0000

    laurafirst commit

---
+++ prime.py
@@ -0,0 +1,17 @@
+someVal = False
+a = 0;
+while (a<1000):
+    print(a)
+    for i in xrange(2,a):
+        if(a%i==0):
+            someVal = False;
+            break;
+        else:
+            someVal = True;
+        
+    if(someVal == True):
+        print("It's a prime")
+    else:
+        print("It's not a prime")
+    a = a + 4
+            
